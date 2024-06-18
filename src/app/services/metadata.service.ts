4b00aee943e738de90a625f26b8d222d063e3509
donia ben sedrine <bensedrinedonia@gmail.com>
2024-06-18 13:15:28 +0100

    updated the login+service

--- src/app/services/metadata.service.ts
+++ src/app/services/metadata.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MetadataService {
 
-  private gatewayUrl = 'http://localhost:8001/'; // URL of the gateway
-
   constructor(private http: HttpClient) { }
 
-  getMetadata(): Observable<any> {
-    const headers = new HttpHeaders().set('Authorization', 'Bearer valid_token');
-    return this.http.get<any>(this.gatewayUrl);
+  getMetadata(token: string): Observable<any> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get<any>(`${environment.gatewayUrl}`, { headers });
   }
 }
