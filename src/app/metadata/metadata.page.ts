4ae04698279af5662db7c596c13b067e99ef34da
donia ben sedrine <bensedrinedonia@gmail.com>
2024-06-23 13:45:09 +0100

    enhanced the appearence

--- src/app/metadata/metadata.page.ts
+++ src/app/metadata/metadata.page.ts
@@ -9,22 +9,26 @@ import { MetadataService } from '../services/metadata.service';
 export class MetadataPage implements OnInit {
   metadata: any;
   lastMatchingMetadata: any = null;
+  metadataKeys: string[] = [];
 
   constructor(private metadataService: MetadataService) {}
 
   ngOnInit(): void {
     this.metadataService.getMetadata().subscribe(
       data => {
-        if (data) {
+        if (data && data !== this.lastMatchingMetadata) {
           this.metadata = data;
           this.lastMatchingMetadata = data; // Store the last matching metadata
+          this.metadataKeys = Object.keys(this.metadata); // Extract keys
           console.log('Metadata received:', this.metadata);
         } else if (this.lastMatchingMetadata) {
           console.log('Using last matching metadata');
           this.metadata = this.lastMatchingMetadata; // Use the last matching metadata
+          this.metadataKeys = Object.keys(this.metadata); // Extract keys
         } else {
           console.warn('No matching metadata found or error occurred');
           this.metadata = null; // Set metadata to null if no match or error and no last matching metadata
+          this.metadataKeys = [];
         }
       },
       error => console.error('Error fetching metadata:', error)
